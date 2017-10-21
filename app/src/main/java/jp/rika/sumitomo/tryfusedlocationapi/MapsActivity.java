e01499e2d9cd36f5da185313a7075df18ea1d67e
rikasumitomo <lmcmacf@gmail.com>
2017-10-21 15:19:39 +0900

    写真からexif取得を追加した. 写真が撮られた位置に青いピンを表示.

--- app/src/main/java/jp/rika/sumitomo/tryfusedlocationapi/MapsActivity.java
+++ app/src/main/java/jp/rika/sumitomo/tryfusedlocationapi/MapsActivity.java
@@ -3,6 +3,7 @@ package jp.rika.sumitomo.tryfusedlocationapi;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.media.ExifInterface;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -19,8 +20,12 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
@@ -36,6 +41,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private int priority[] = {LocationRequest.PRIORITY_HIGH_ACCURACY, LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY,
             LocationRequest.PRIORITY_LOW_POWER, LocationRequest.PRIORITY_NO_POWER};
     private int locationPriority;
+
+    //exif
+    private double latitudeA;
+    private double longitudeA;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,6 +87,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
+
+        //assetsフォルダの写真からexif取得
+        try {
+            InputStream stream_in = this.getResources().getAssets().open("sample.JPG");
+            ExifInterface exif = new ExifInterface(stream_in);
+
+            if (exif != null) {
+                String latitude = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE);//緯度
+                String latitudeRef = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE_REF);//北緯or南緯
+                String longitude = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);//経度
+                String longitudeRef = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF);//東経or西経
+
+                //時,分,秒（60進数)
+                Log.d("exif", "latitude : " + latitude);
+                Log.d("exif", "latitudeRef : " + latitudeRef);
+                Log.d("exif", "longitude : " + longitude);
+                Log.d("exif", "longitudeRef : " + longitudeRef);
+
+                //10進数に変換
+                latitudeA = ExifHourMinSecToDegreesLatitude(latitude);
+                longitudeA = ExifHourMinSecToDegreesLongitude(longitude);
+
+                Log.d("DegreeExif","latitude : " + ExifLatitudeToDegrees(latitudeRef,latitude));
+                Log.d("DegreeExif","longitude : " + ExifLongitudeToDegrees(longitudeRef,longitude));
+
+            }
+        } catch (IOException e) {
+            Log.d("exif","exif is null");
+            e.printStackTrace();
+        }
     }
 
     // onResumeフェーズに入ったら接続
@@ -99,12 +139,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // check permission
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
                 || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Log.d("debug", "permission granted");
+            Log.d("debug", "permission granted１");
 
             mMap = googleMap;
             mMap.setLocationSource(this);
             mMap.setMyLocationEnabled(true);//右上の現在位置アイコンの表示
             mMap.setOnMyLocationButtonClickListener(this);
+            //写真のマーカーをセット
+            setMarker();
         }
         else{
             Log.d("debug", "permission error");
@@ -138,7 +180,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // check permission
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
                 || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Log.d("debug", "permission granted");
+            Log.d("debug", "permission granted２");
 
             // FusedLocationApi
             LocationServices.FusedLocationApi.requestLocationUpdates(
@@ -176,5 +218,58 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void deactivate() {
         this.onLocationChangedListener = null;
+    }
+
+
+
+    //写真の位置情報マーカー
+    private void setMarker(){
+        //マーカーを追加
+        LatLng markerPos = new LatLng(latitudeA,longitudeA);
+        MarkerOptions options = new MarkerOptions();
+        options.position(markerPos)
+                .title("Exif")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        //.icon(BitmapDescriptorFactory.fromResource(R.drawable.sample));
+
+
+        mMap.addMarker(options);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markerPos, 10));//10:都市 //15:街路
+
+    }
+
+    //以下Exif情報の取得変換 ****************************************************
+    private double ExifHourMinSecToDegreesLatitude(String latitudE) {
+        String hourminsec[] = latitudE.split(",");
+        String hour[] = hourminsec[0].split("/");
+        String min[] = hourminsec[1].split("/");
+        String sec[] = hourminsec[2].split("/");
+        double dhour = (double)Integer.parseInt(hour[0]) / (double)Integer.parseInt(hour[1]);
+        double dmin = (double)Integer.parseInt(min[0]) / (double)Integer.parseInt(min[1]);
+        double dsec = (double)Integer.parseInt(sec[0]) / (double)Integer.parseInt(sec[1]);
+        double degrees = dhour + dmin / 60.0 + dsec / 3600.0;
+        return degrees;
+    }
+
+    private double ExifHourMinSecToDegreesLongitude(String longitudE) {
+        String hourminsec[] = longitudE.split(",");
+        String hour[] = hourminsec[0].split("/");
+        String min[] = hourminsec[1].split("/");
+        String sec[] = hourminsec[2].split("/");
+        double dhour = (double)Integer.parseInt(hour[0]) / (double)Integer.parseInt(hour[1]);
+        double dmin = (double)Integer.parseInt(min[0]) / (double)Integer.parseInt(min[1]);
+        double dsec = (double)Integer.parseInt(sec[0]) / (double)Integer.parseInt(sec[1]);
+        double degrees = dhour + dmin / 60.0 + dsec / 3600.0;
+        return degrees;
+    }
+
+    private String ExifLatitudeToDegrees(String ref, String latitudE) {
+        String answer = String.valueOf(ref.equals("S") ? -1.0 : 1.0 * ExifHourMinSecToDegreesLatitude(latitudE));
+        return answer;
+    }
+
+    private String ExifLongitudeToDegrees(String ref, String longitudE) {
+        String answer = String.valueOf(ref.equals("W") ? -1.0 : 1.0 * ExifHourMinSecToDegreesLongitude(longitudE));
+        return answer;
     }
 }
